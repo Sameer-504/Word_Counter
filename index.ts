f1e8c19410c9629a3c92a87d693be4c82a9644f7
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-04-21 14:01:04 +0500

    Word Counter
    This is a Word Counter Project

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+//Import the 'Inquirer, module, which is a command line interface for Node.js
+import inquirer from "inquirer"
+import chalk from "chalk";
+
+
+console.log(chalk.greenBright.bold("\n \t Wellcome to the code with Sameer Anis' - Word Counter\n"));
+
+//Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+
+const answers: {
+    sentence: string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence to Count the Word: "
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+
+// Print the array of words tot he console
+
+console.log(words)
+
+// Print the word count of the sentence to the console
+
+console.log(`Your Sentence word count is ${words.length}`);
+
+
+
